942ab57717b1fb81295d93e63930554d60c1cb55
Deem <95542707+DeemOrainy@users.noreply.github.com>
2024-12-31 23:22:23 +0300

    Update depressionModel.py

--- depressionModel.py
+++ depressionModel.py
@@ -4,23 +4,21 @@ import joblib
 # تحميل النموذج
 model = joblib.load("depression_risk_model1.pkl")
 
-# إضافة CSS لتوسيط النصوص
 st.markdown(
     """
     <style>
-    .centered-title {
-        text-align: center;
-        font-size: 30px;
-        font-weight: bold;
+    .right-align {
+        text-align: right;
+        font-size: 18px;
     }
     </style>
     """,
     unsafe_allow_html=True,
 )
 
-# عنوان التطبيق مع التوسيط
-st.markdown('<div class="centered-title">تقييم خطر الاكتئاب بناءً على وقت استخدام الجهاز</div>', unsafe_allow_html=True)
-
+# إدخال عدد الدقائق مع محاذاة لليمين
+st.markdown('<div class="right-align">أدخل عدد دقائق استخدام جهازك في اليوم</div>', unsafe_allow_html=True)
+app_usage = st.number_input("", min_value=0)
 # إدخال عدد الدقائق
 app_usage = st.number_input("أدخل عدد دقائق استخدام جهازك في اليوم", min_value=00)
 
