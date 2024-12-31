e05cf26ed095e862581f90e34cbf9eccc272b67f
Deem <95542707+DeemOrainy@users.noreply.github.com>
2024-12-31 23:13:21 +0300

    Add files via upload

---
+++ depressionModel.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import joblib
+
+# تحميل النموذج
+model = joblib.load("depression_risk_model1.pkl")
+
+# إضافة CSS لتوسيط النصوص
+st.markdown(
+    """
+    <style>
+    .centered-title {
+        text-align: center;
+        font-size: 30px;
+        font-weight: bold;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# عنوان التطبيق مع التوسيط
+st.markdown('<div class="centered-title">تقييم خطر الاكتئاب بناءً على وقت استخدام الجهاز</div>', unsafe_allow_html=True)
+
+# إدخال عدد الدقائق
+app_usage = st.number_input("أدخل عدد دقائق استخدام التطبيق في اليوم:", min_value=00)
+
+# زر التقييم
+if st.button("تقييم"):
+    user_data = [[app_usage]]
+    prediction = model.predict(user_data)
+    if prediction[0] == 1:
+        st.write("⚠️ هناك احتمال لإصابتك بالاكتئاب.")
+    else:
+        st.write("✅ لا يوجد خطر للإصابة بالاكتئاب.")
---
+++ model.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+df = pd.read_csv("user_behavior_dataset.csv")
+
+df.dropna(inplace=True)
+
+df['Depression_Risk'] = df['User Behavior Class'].apply(lambda x: 1 if x == 5 else 0)
+
+X = df[['App Usage Time (min/day)']]  
+y = df['Depression_Risk'] 
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
+
+import joblib
+
+# حفظ النموذج
+joblib.dump(model, "depression_risk_model1.pkl")
+print("Model saved as depression_risk_model1.pkl")
