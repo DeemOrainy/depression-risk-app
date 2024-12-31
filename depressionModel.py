8f29455ddf128cd50935cc58a8732b2b44e02b6e
Deem <95542707+DeemOrainy@users.noreply.github.com>
2024-12-31 23:25:59 +0300

    Update depressionModel.py

--- depressionModel.py
+++ depressionModel.py
@@ -4,7 +4,7 @@ import joblib
 # تحميل النموذج
 model = joblib.load("depression_risk_model1.pkl")
 
-# إضافة CSS لتنسيق العنوان، النصوص، والزر
+# إضافة CSS لتوسيط النصوص
 st.markdown(
     """
     <style>
@@ -12,15 +12,6 @@ st.markdown(
         text-align: center;
         font-size: 30px;
         font-weight: bold;
-    }
-    .right-align {
-        text-align: right;
-        font-size: 18px;
-    }
-    .center-button {
-        display: flex;
-        justify-content: center;
-        margin-top: 20px;
     }
     </style>
     """,
@@ -30,14 +21,10 @@ st.markdown(
 # عنوان التطبيق مع التوسيط
 st.markdown('<div class="centered-title">تقييم خطر الاكتئاب بناءً على وقت استخدام الجهاز</div>', unsafe_allow_html=True)
 
-# إدخال عدد الدقائق مع محاذاة لليمين
-st.markdown('<div class="right-align">أدخل عدد دقائق استخدام جهازك في اليوم</div>', unsafe_allow_html=True)
-app_usage = st.number_input("", min_value=0)
+# إدخال عدد الدقائق
+app_usage = st.number_input("أدخل عدد دقائق استخدام جهازك في اليوم", min_value=00)
 
-# زر التقييم في الوسط
-st.markdown('<div class="center-button"><button type="button">تقييم</button></div>', unsafe_allow_html=True)
-
-# تنفيذ المنطق عند النقر على الزر
+# زر التقييم
 if st.button("تقييم"):
     user_data = [[app_usage]]
     prediction = model.predict(user_data)
