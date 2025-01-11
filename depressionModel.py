3f6d69fd060c181fc9474d5b20a20f486fd603b2
Deem <95542707+DeemOrainy@users.noreply.github.com>
2025-01-11 14:38:35 +0300

    Update depressionModel.py

--- depressionModel.py
+++ depressionModel.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 
 # تحميل النموذج
-model = joblib.load("depression_risk_model1.pkl")
+model = joblib.load("depression_risk_modelf.pkl")
 
 # إضافة CSS لتوسيط النصوص
 st.markdown(
@@ -27,19 +27,24 @@ st.markdown(
     unsafe_allow_html=True,
 )
 # عنوان التطبيق مع التوسيط
-st.markdown('<div class="centered-title">تقييم خطر الاكتئاب بناءً على وقت استخدام الجهاز</div>', unsafe_allow_html=True)
+st.markdown('<div class="centered-title">تقييم خطر الاكتئاب بناءً على وقت استخدام برامج التواصل الاجتماعي اليومي</div>', unsafe_allow_html=True)
 
+# إدخال عدد الساعات مع محاذاة لليمين
+st.markdown('<div class="right-align">أدخل ساعات استخدامك لبرامج التواصل الاجتماعي في اليوم</div>', unsafe_allow_html=True)
+app_usage_hours = st.number_input("عدد الساعات", min_value=0.0, step=0.1)  # إدخال عدد الساعات كرقم عشري
 
-# إدخال عدد الدقائق مع محاذاة لليمين
-st.markdown('<div class="right-align">أدخل عدد دقائق استخدام جهازك في اليوم</div>', unsafe_allow_html=True)
-app_usage = st.number_input("", min_value=0)
-
+# تحويل الساعات إلى دقائق
+app_usage_minutes = app_usage_hours * 60
 
 # زر التقييم
 if st.button("تقييم"):
-    user_data = [[app_usage]]
+    user_data = [[app_usage_minutes]]
     prediction = model.predict(user_data)
     if prediction[0] == 1:
         st.markdown('<div class="right-align">⚠️ هناك احتمال لإصابتك بالاكتئاب</div>', unsafe_allow_html=True)
     else:
         st.markdown('<div class="right-align">✅ لا يوجد خطر للإصابة بالاكتئاب</div>', unsafe_allow_html=True)
+
+# تعليمات الاستخدام
+st.markdown('<div class="right-align">:تعليمات الإستخدام للحصول على عدد الساعات</div>', unsafe_allow_html=True)
+st.markdown('<div class="right-align">الدخول على إعدادات الجهاز -> مدة استخدام الجهاز -> إظهار جميع التطبيقات -> الضغط على إظهار الفئات -> الضغط على التواصل الإجتماعي لإظهار تفاصيل المعدل اليومي</div>', unsafe_allow_html=True)
